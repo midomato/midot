2c9c52ee2ff87dc1be00a76a4b084a07d002ab9d
midomato <midotomato@gmail.com>
2025-10-02 13:52:41 +0900

    headerがすごいことに

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <p>私はははははは</p>
+    </>
+  );
+}
--- src/components/templates/Header.tsx
+++ src/components/templates/Header.tsx
@@ -3,8 +3,8 @@ import TweetButton from "../organisms/TweetButton";
 export default function Header() {
   return (
     <header
-      className="flex fixed top-3 gap-10 w-full h-25 z-100 p-6 rounded-4xl border border-white/10 
-                bg-gradient-to-br from-white/60 via-cyan-200/30 to-blue-300/10
+      className="flex fixed top-3 gap-10 w-full h-25 z-100 p-6 rounded-4xl 
+                bg-gradient-to-br from-white/60 via-cyan-800/30 to-gray-400/30
                 backdrop-blur-sm shadow-lg shadow-blue-500/10 text-white font-bold"
     >
       <a
@@ -13,13 +13,22 @@ export default function Header() {
       >
         ホーム
       </a>
-      <a className="shadow-blue-950/90 shadow-lg w-20 rounded-3xl flex items-center justify-center">
+      <a
+        href="/profile"
+        className="shadow-blue-950/90 shadow-lg w-20 rounded-3xl flex items-center justify-center"
+      >
         プロフィール
       </a>
-      <a className="shadow-blue-950/90 shadow-lg w-20 rounded-3xl flex items-center justify-center">
+      <a
+        href="/posts"
+        className="shadow-blue-950/80 shadow-lg w-20 rounded-3xl flex items-center justify-center"
+      >
         ブログ
       </a>
-      <a className="shadow-blue-950/90 shadow-lg w-20 rounded-3xl flex items-center justify-center">
+      <a
+        href="/projects"
+        className="shadow-blue-950/90 shadow-lg w-20 rounded-3xl flex items-center justify-center"
+      >
         成果物
       </a>
       <TweetButton />
